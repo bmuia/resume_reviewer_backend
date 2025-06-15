d7c1390184b8912eb835e960c0735f53dc3eeeef
Bmuia <belammuia0@gmail.com>
2025-06-15 09:37:33 -0400

    fix deployment bugs

--- resume_reviewer_backend/settings.py
+++ resume_reviewer_backend/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'https://resume-reviewer-frontend.vercel.app/'
+    'https://resume-reviewer-frontend.vercel.app'
 ]
 
 
